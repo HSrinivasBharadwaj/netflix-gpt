8637d9598353041d9c4a6507455bd55a95283ff6
HSrinivasBharadwaj <srinivasbharadwaj.hullur@gmail.com>
2024-04-05 18:10:42 +0530

    Final changes

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,6 +2,7 @@ import React from "react";
 import { IMAGE_CDN } from "../utils/constants";
 
 const MovieCard = ({ movie }) => {
+  if (!movie.poster_path) return null;
   return (
     <div className="mr-4">
       <img className="w-48 max-w-none rounded cursor-pointer" src={IMAGE_CDN + movie.poster_path} />
